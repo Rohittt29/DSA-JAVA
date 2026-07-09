6e5fce4bd67e42b55842024af9018f5f47e6affd
Rohittt29 <rohitkumbhar1229@gmail.com>
2026-07-09 14:02:00 +0530

    Checking if the given array is sorted or not

--- test.java
+++ test.java
@@ -1,14 +1,21 @@
-public class test {
+public class test{
     public static void main(String[]args){
-    //finding the largest number :- 
-    int arr [] = {57,1,0,10,99,3,2,6,104,7};
-    int n = arr.length;
-    int max = arr[0];
-    for (int i = 0 ; i < n ; i++){
-        if(arr[i] > max){
-            max = arr[i];
+        int [] arr = {3,1,2,5,9,7,8};
+        int n = arr.length;
+
+        int largest = arr[0];
+        int secondlargest = Integer.MIN_VALUE;
+        
+        for(int i = 0 ; i < n ; i++){
+            if(arr[i] > largest){
+                
+                secondlargest = largest;
+                largest  = arr[i];
+            }
+            else if (arr[i] > secondlargest && arr[i] != largest){
+                secondlargest = arr[i];
+            }
         }
+        System.out.println("The second largest element is :" + secondlargest);
     }
-    System.out.println("MAX :" + max);
-}
 }
