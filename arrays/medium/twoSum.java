8ca20d88cd7dc4ac3562186f6396b949594ca4d9
Rohittt29 <rohitkumbhar1229@gmail.com>
2026-07-27 23:16:42 +0530

    used the hashmap in the medium questioni number 1 that is two sum problem

--- arrays/medium/twoSum.java
+++ arrays/medium/twoSum.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class twoSum {
@@ -6,43 +7,40 @@ public class twoSum {
 
         Scanner sc = new Scanner(System.in);
 
-        // Input array size
-        System.out.print("Enter the size of the array: ");
+        // Input size of array
+        System.out.print("Enter size of array: ");
         int n = sc.nextInt();
 
         int[] nums = new int[n];
 
         // Input array elements
-        System.out.println("Enter the array elements:");
+        System.out.println("Enter array elements:");
         for (int i = 0; i < n; i++) {
             nums[i] = sc.nextInt();
         }
 
         // Input target
-        System.out.print("Enter the target: ");
+        System.out.print("Enter target: ");
         int target = sc.nextInt();
 
-        // Brute Force Solution
-        boolean found = false;
+        // Create HashMap
+        HashMap<Integer, Integer> map = new HashMap<>();
 
+        // Two Sum Logic
         for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
 
-                if (nums[i] + nums[j] == target) {
-                    System.out.println("Indices are: " + i + " " + j);
-                    found = true;
-                    break;
-                }
+            int need = target - nums[i];
+
+            if (map.containsKey(need)) {
+                System.out.println("Indices: " + map.get(need) + " " + i);
+                sc.close();
+                return;
             }
 
-            if (found) {
-                break;
-            }
+            map.put(nums[i], i);
         }
 
-        if (!found) {
-            System.out.println("No pair found.");
-        }
+        System.out.println("No pair found.");
 
         sc.close();
     }
