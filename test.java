ba7a3915f33e1166426b9803197bbb0e56fca112
Rohittt29 <rohitkumbhar1229@gmail.com>
2026-08-11 13:32:50 +0530

    rewrite the hashmap code

--- test.java
+++ test.java
@@ -1,11 +1,21 @@
+import java.util.*;
 public class test{
-     public static void main(String []args){
-          int [] arr1 = {1, 2, 3, 4, 5};
-          int [] arr2 = {1, 2, 7};
-          int n1 = arr1.length;
-          int n2 = arr2.length;
+     public int[] twoSum(int[] nums, int target){
+          HashMap<Integer,Integer> map = new HashMap<>();
+          int n = nums.length;
+          for(int i = 0; i < n; i++){
+               int need = target - nums[i];
+               if(map.containsKey(need)){
+                    //if yes then return the ans:-
+                    return new int[]{map.get(need), i};
+               }
+               //if no then store the current that is nums[i] in the map:-
+               map.put(nums[i], i);
+          }
+          return new int[]{};
+     }
 
-          
+     public static void main(String[]args){
 
      }
 }
