a1cbc5c0b47357b8cee718d1bf08c8ab6bc5daa3
Rohittt29 <rohitkumbhar1229@gmail.com>
2026-08-11 23:26:05 +0530

    clearing things off

--- arrays/medium/Kadanes.java
+++ arrays/medium/Kadanes.java
@@ -1,21 +1,5 @@
 import java.util.*;
 class Solution {
-    public int Kadanes(int[] nums) {
-        if (nums == null || nums.length == 0) {
-            return 0;
-        }
-        int maxEndingHere = nums[0];
-        int maxSoFar = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            maxEndingHere = Math.max(nums[i], maxEndingHere + nums[i]);
-            maxSoFar = Math.max(maxSoFar, maxEndingHere);
-        }
-        return maxSoFar;
-    }
-
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
-        System.out.println(sol.Kadanes(nums));
-    }
+    //public int Kadanes(int[] nums) {
+        
 }
