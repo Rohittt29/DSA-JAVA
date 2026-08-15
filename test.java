00cdb4db62015947d770c640ea6830fa744b9730
Rohittt29 <rohitkumbhar1229@gmail.com>
2026-08-15 22:23:18 +0530

    commit2

--- test.java
+++ test.java
@@ -1,21 +1,6 @@
 import java.util.*;
 public class test{
-     public int[] twoSum(int[] nums, int target){
-          HashMap<Integer,Integer> map = new HashMap<>();
-          int n = nums.length;
-          for(int i = 0; i < n; i++){
-               int need = target - nums[i];
-               if(map.containsKey(need)){
-                    //if yes then return the ans:-
-                    return new int[]{map.get(need), i};
-               }
-               //if no then store the current that is nums[i] in the map:-
-               map.put(nums[i], i);
-          }
-          return new int[]{};
-     }
-
-     public static void main(String[]args){
-               System.out.println("hello world ");
+     public static void main(String []args){
+          System.out.println("hello world");
      }
 }
